ab130197184537888c592ab07237eb70147246fb
0x00-pl <0x00.pl@gmail.com>
2018-04-05 16:48:09 +0800

    add code

--- src/vm.hpp
+++ src/vm.hpp
@@ -1,12 +1,59 @@
 #pragma once
 #include <memory>
 #include <vector>
+#include "ac_datatype/ac_int.h"
+#include <bitset>
 #include <experimental/optional>
 
 namespace pl_vm
 {
 using namespace std;
 using namespace experimental;
+
+template<typename T>
+optional<string> data_signals(const optional<T>& t){
+    if(t){
+        return to_string(t.value());
+    } else {
+        return {};
+    }
+}
+
+template<int W, bool S>
+optional<string> data_signals(const optional<ac_int<W,S>>& t) {
+    if(t){
+        bitset<W> b = *(bitset<W>*)&t;
+        return "b" + to_string(b);
+    } else {
+        return {};
+    }
+}
+
+template<int W, int I, bool S, ac_q_mode Q, ac_o_mode O>
+optional<string> data_signals(const optional<ac_fixed<W,I,S,Q,O>>& t){
+    if(t){
+        bitset<W> b = *(bitset<W>*)&t;
+        return "b" + to_string(b);
+    } else {
+        return {};
+    }
+}
+
+template<typename T>
+struct data_width{
+    static const size_t value = sizeof(T)*8;
+};
+
+template<int W, bool S>
+struct data_width<ac_int<W,S>>{
+    static const size_t value = W;
+};
+
+template<int W, int I, bool S, ac_q_mode Q, ac_o_mode O>
+struct data_width<ac_fixed<W,I,S,Q,O>>{
+    static const size_t value = W;
+};
+
 
 class hd_ticker{
 public:
@@ -17,13 +64,13 @@ public:
 class hd_unit : public hd_ticker {
 public:
     virtual ~hd_unit(){}
-    void tick ( int ) override = 0;
 };
 
 class hd_wire_base : public hd_ticker {
 public:
     virtual ~hd_wire_base(){}
-    void tick ( int ) override = 0;
+    virtual optional<string> signals(){ return {}; }
+    virtual size_t width(){ return 0; }
 };
 
 template<typename T>
@@ -34,7 +81,14 @@ public:
     hd_wire():data{}{}
     virtual ~hd_wire(){}
     void tick ( int ) override {}
+    optional<string> signals() override {
+        return data_signals(data);
+    }
+    size_t width() override{
+        return data_width<Tdata>::value;
+    }
 };
+
 
 class vm {
 public:
